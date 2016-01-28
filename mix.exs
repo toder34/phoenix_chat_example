b6904d4a7aff311eca86b1fff2ce461cc8225f3f
Troels Brødsgaard <tr@arbr.dk>
2016-01-28 14:40:32 +0100

    update to Phoenix 1.1

--- mix.exs
+++ mix.exs
@@ -22,11 +22,12 @@ defmodule Chat.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.0.0"},
-     {:phoenix_html, "~> 2.1"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:phoenix_ecto, "~> 1.1"},
+    [{:phoenix, "~> 1.1.4"},
      {:postgrex, ">= 0.0.0"},
+     {:phoenix_ecto, "~> 2.0"},
+     {:phoenix_html, "~> 2.4"},
+     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
      {:telly, github: "trarbr/telly"}]
   end
